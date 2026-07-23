e954e2e9a20e77bbc415109afe9b052e091ba5a2
Shurropa <jackbraun091@gmail.com>
2026-07-23 02:24:26 -0400

    weapon

--- Scripts/Player/Player_Movement.gd
+++ Scripts/Player/Player_Movement.gd
@@ -1,13 +1,14 @@
 extends CharacterBody3D
 
 
-const SPEED = 12.0
+const SPEED = 9.0
 const UP = 2
 const DOWN = 4
+const LOCK_X: float = 17
+const LOCK_Z: float = 9.3
 
 
 func _physics_process(_delta: float) -> void:
-	
 	
 	var directionX := Input.get_axis("move_left", "move_right")
 	var directionZ := Input.get_axis("move_forward", "move_backward")
@@ -20,4 +21,7 @@ func _physics_process(_delta: float) -> void:
 	else:
 		velocity.z = move_toward(velocity.z, 0, DOWN)
 
+	
 	move_and_slide()
+	position.x = clampf(position.x,-LOCK_X,LOCK_X)
+	position.z = clampf(position.z,-LOCK_Z -1 ,LOCK_Z)
--- Scripts/Resources/Weapon.gd
+++ Scripts/Resources/Weapon.gd
@@ -6,8 +6,8 @@ enum attackSpeeds {VerySlow,Slow,Normal,Fast,VeryFast,UltraMegaTikiPhonkKindaSpe
 enum rarities {Common, Uncommon, Rare, Legendary, Mythical, TungTungShanor}
 
 @export var name: String
-@export var maxLight: int
-@export var damage: int
-@export var attackSpeed: attackSpeeds
-@export var rarity: rarities
+@export var maxLight: int = 2
+@export var damage: int = 5
+@export var attackSpeed: attackSpeeds = attackSpeeds.Normal
+@export var rarity: rarities = rarities.Common
 @export var ability: String
