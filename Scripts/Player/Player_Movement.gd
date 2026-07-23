a468ce59b43c1f347e1005837cfb55686758ac18
Mailbox-Fertilizer <TheDragon872@hotmail.com>
2026-07-23 00:27:58 -0400

    player code

--- Scripts/Player/Player_Movement.gd
+++ Scripts/Player/Player_Movement.gd
@@ -1,20 +1,23 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-
+const SPEED = 12.0
+const UP = 2
+const DOWN = 4
 
 
 func _physics_process(_delta: float) -> void:
 	
 	
-	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	var directionX := Input.get_axis("move_left", "move_right")
+	var directionZ := Input.get_axis("move_forward", "move_backward")
+	if directionX != 0:
+		velocity.x = move_toward(velocity.x, directionX * SPEED, UP)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, DOWN)
+	if directionZ != 0:
+		velocity.z = move_toward(velocity.z, directionZ * SPEED, UP)
+	else:
+		velocity.z = move_toward(velocity.z, 0, DOWN)
 
 	move_and_slide()
