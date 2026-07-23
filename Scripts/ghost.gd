3531b69bc6c61a0a3896343da96bcbcfc6048943
Shurropa <jackbraun091@gmail.com>
2026-07-23 12:32:05 -0400

    Monster AI
    Pathfinding to player

--- Scripts/ghost.gd
+++ Scripts/ghost.gd
@@ -1,6 +1,10 @@
 extends CharacterBody3D
+class_name Enemy
 @onready var player = $"../Player"
 const SPEED = 5
+const LOCK_X: float = 17
+const LOCK_Z: float = 9.3
+
 var direction = 1
 var randX = randi_range(-30,30)
 var randZ = randi_range(-30,30)
@@ -13,15 +17,17 @@ func _physics_process(_delta):
 	# + Vector3(randX,0,randZ)
 	
 	velocity = direction * SPEED
-	
+	look_at(player.position,Vector3.UP) 
 	
 
-		
-		
-		
-		
-		
-		
-		
-		
+	
+	
+	
+	
+	
+	
+	
+	
 	move_and_slide()
+	position.x = clampf(position.x,-LOCK_X,LOCK_X)
+	position.z = clampf(position.z,-LOCK_Z -1 ,LOCK_Z)
