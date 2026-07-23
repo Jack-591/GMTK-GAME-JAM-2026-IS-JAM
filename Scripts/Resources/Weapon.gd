7dccae29429b9b03f3901ccbdbade2d023ca0998
Mailbox-Fertilizer <TheDragon872@hotmail.com>
2026-07-23 12:56:22 -0400

    enum list

--- Scripts/Resources/Weapon.gd
+++ Scripts/Resources/Weapon.gd
@@ -2,10 +2,17 @@ extends Resource
 
 class_name Weapon
 
-enum attackSpeeds {VerySlow,Slow,Normal,Fast,VeryFast,UltraMegaTikiPhonkKindaSpeed}
-enum rarities {Common, Uncommon, Rare, Legendary, Mythical, TungTungShanor}
 
-@export var name: String
+enum weapons {BasicLamp,Candle,OiledLamp,Camera,Torch,FlareGun,BlackLight}
+
+
+enum attackSpeeds {VerySlow,Slow,Normal,Fast,VeryFast,UltraMegaTikiPhonkKindaSpeed}
+enum rarities {Common, Rare, Legendary, TungTungShanor}
+
+
+
+
+@export var name: weapons
 @export var maxLight: int = 2
 @export var damage: int = 5
 @export var attackSpeed: attackSpeeds = attackSpeeds.Normal
