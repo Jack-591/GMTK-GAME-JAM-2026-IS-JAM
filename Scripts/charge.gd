b6eed51344e83fe62f327f8cf4d32ee34255e790
Shurropa <jackbraun091@gmail.com>
2026-07-24 04:08:35 -0400

    thingy

--- Scripts/charge.gd
+++ Scripts/charge.gd
@@ -15,10 +15,14 @@ const LOCK_Z: float = 9.3
 var direction = 1
 var randX = randi_range(-30,30)
 var randZ = randi_range(-30,30)
-var health
+var health: float
 
+var dashSpeed: float = 20
+var dashRange: float = 10
 
-var dash = false
+var preparingDash = false
+var dashing = false
+var dashCoolDown = false
 
 
 func _ready() -> void:
@@ -32,50 +36,47 @@ func _physics_process(_delta):
 	#print(player.global_position.z)
 	direction = ((player.global_position) - global_position).normalized()
 	
-	#if dash == false:
-		#velocity = direction * speed
-	#else:
-		#velocity = Vector3(0,0,0)
-#
-	#if dashCool == false and (global_position.distance_squared_to(player.global_position) < 10 or dash == true):
-		#if dash == false:
-			#$startup.start()
-			#dash = true
-		#if dashing == false:
-			#velocity = direction * 20
-	#else:
-		#look_at(player.position,Vector3.UP) 
-	
-	if (global_position.distance_squared_to(player.global_position) < 10) and dash == false:
-		print("dashing")
-		dash = true
+	if dashing:
 		velocity = direction * 20
-		await get_tree().create_timer(2).timeout
-		dash = false
-		look_at(player.position,Vector3.UP) 
+		look_at(player.global_position,Vector3.UP)
+	elif preparingDash:
+		velocity = Vector3.ZERO
 	else:
-		look_at(player.position,Vector3.UP)
 		velocity = direction * speed
+		
+		if not dashCoolDown:
+			var distance = global_position.distance_squared_to(player.global_position)
+			
+			if distance < dashRange * dashRange:
+				print("starting to dash")
+				startDash()
+	if not preparingDash and not dashing:
+		look_at(player.global_position,Vector3.UP)
+	
 	
 	move_and_slide()
 	position.x = clampf(position.x,-LOCK_X,LOCK_X)
 	position.z = clampf(position.z,-LOCK_Z -1 ,LOCK_Z)
 	
 
+func startDash() -> void:
+	preparingDash = true
+	velocity = Vector3.ZERO
+	$startup.start()
 
-#func _on_startup_timeout():
-	#dashing = true
-	#$dash.start()
-	#
-	#
-	#
-#func _on_dash_timeout():
-	#dashing = false
-	#dash = false
-	#dashCool = true
-	#$cooldown.start()
-#
-#
-#func _on_cooldown_timeout():
-	#dashCool = false
+func _on_startup_timeout():
+	preparingDash = false
+	dashing = true
+	$dash.start()
+	
+	
+	
+func _on_dash_timeout():
+	dashing = false
+	dashCoolDown = true
+	$cooldown.start()
+
+
+func _on_cooldown_timeout():
+	dashCoolDown = false
 	
